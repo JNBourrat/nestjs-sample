84f26d2673aa0e1eb9854a22899ae222bf12c63c
JNBourrat <jnbourrat.dev@gmail.com>
2019-04-11 15:10:18 +0200

    added clean-webpack-plugin to clean dist folder before starting HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: ['webpack/hot/poll?100', './src/main.ts'],
@@ -22,7 +23,11 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
-  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/])],
+  plugins: [
+    new CleanWebpackPlugin({}),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/])
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'server.js',
